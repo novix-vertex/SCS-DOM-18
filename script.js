9f2ad0a0236b76b0bb32b7970cc7b50e0e77e124
chirag jain <jchirag0610@gmail.com>
2026-04-14 17:40:16 +0530

    code corrected

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-main = document.querySelector("main");
-thor = document.querySelector(".thor");
-gorr = document.querySelector(".gorr");
+let main = document.querySelector("main");
+let thor = document.querySelector(".thor");
+let gorr = document.querySelector(".gorr");
 
-let interval = setInterval(function () {
+
+let state = 0;
+
+setTimeout(function () {
     gorr.style.top = "0%";
 
 }, 1000);
@@ -11,42 +14,67 @@ let horror = new Audio("horror.mp3");
 let thunder = new Audio("thunder.mp3");
 let scream = new Audio("scream.mp3");
 
-horror.muted = true;
-thunder.muted = true;
-scream.muted = true;
-
 horror.loop = true;
 thunder.loop = true;
 scream.loop = true;
 
 main.addEventListener("click", function () {
-    horror.muted = false;
-    horror.play();
-});
 
-setTimeout(function () {
+    if (state == 0) {
 
-    main.addEventListener("click", function () {
+        horror.play();
+        setTimeout(function () {
+            state = 1;
+        }, 3000);
+    }
+
+    if (state == 1) {
+
         document.body.style.background = "url('https://i.pinimg.com/originals/38/8b/e2/388be24848b1bbb4388a4352f0af1a5c.gif')";
         document.body.style.backgroundPosition = "center";
         document.body.style.backgroundSize = "cover";
 
         horror.muted = true;
         horror.pause();
-        thunder.muted = false;
+
         thunder.play();
 
-        setTimeout(function () {
-            thor.style.opacity = 1;
-            thor.style.bottom = "200px";
-            thor.style.right = "200px";
-        }, 1000);
+        thor.style.opacity = 1;
+        thor.style.bottom = "200px";
+        thor.style.right = "200px";
+        state = 2;
+    }
 
-    });
+    if (state == 3) {
+        console.log("gorr");
+        if (state == 3) {
+            gorr.style.top = "-50%";
+            thunder.muted = true;
+            thunder.pause();
 
-}, 4000);
+            scream.play();
+            setTimeout(function () {
+                scream.muted = true;
+                scream.pause();
 
-const controller = new AbortController();
+                for (let i = 1; i <= 100; i++) {
+                    spot = document.createElement("img");
+                    spot.src = "https://www.freeiconspng.com/uploads/blood-spatter-high-velocity-blunt-spatter-png-20.png";
+                    spot.style.position = "absolute";
+                    spot.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
+                    spot.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+                    spot.style.height = Math.floor(Math.random() * 200) + "px";
+                    spot.style.width = Math.floor(Math.random() * 200) + "px";
+                    spot.zIndex = -5;
+                    main.appendChild(spot);
+                }
+
+            }, 2000);
+            state = 4;
+        }
+    }
+
+});
 
 
 main.addEventListener("mousemove", function (dets) {
@@ -80,41 +108,24 @@ main.addEventListener("mousemove", function (dets) {
 
 
     if ((thor_x >= gorr_xmin && thor_x <= gorr_xmax) && (thor_y >= gorr_ymin && thor_y <= gorr_ymax)) {
-        // console.log("condition matched");
-        gorr.style.top = "-50%";
-        clearInterval(interval);
-
-        for (let i = 1; i <= 100; i++) {
-            spot = document.createElement("img");
-            spot.src = "https://www.freeiconspng.com/uploads/blood-spatter-high-velocity-blunt-spatter-png-20.png";
-            spot.style.position = "absolute";
-            spot.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
-            spot.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
-            spot.style.height = Math.floor(Math.random() * 200) + "px";
-            spot.style.width = Math.floor(Math.random() * 200) + "px";
-            spot.zIndex = -5;
-            main.appendChild(spot);
+        console.log("condition matched");
+        console.log(state);
+        if (state == 2) {
+            state = 3;
         }
-        thor.style.height = "250px";
-        thor.style.width = "auto";
-        thor.style.top = "100px";
-
-        main.addEventListener("click", function () {
-            thunder.muted = true;
-            thunder.pause();
-
-            scream.muted = false;
-            scream.play();
-            setTimeout(function () {
-                scream.muted = true;
-                scream.pause();
-                controller.abort();
-            }, 2000);
-
-        });
-
-
-
+        // for (let i = 1; i <= 100; i++) {
+        //     spot = document.createElement("img");
+        //     spot.src = "https://www.freeiconspng.com/uploads/blood-spatter-high-velocity-blunt-spatter-png-20.png";
+        //     spot.style.position = "absolute";
+        //     spot.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
+        //     spot.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+        //     spot.style.height = Math.floor(Math.random() * 200) + "px";
+        //     spot.style.width = Math.floor(Math.random() * 200) + "px";
+        //     spot.zIndex = -5;
+        //     main.appendChild(spot);
+        // }
     }
 
-}, { signal: controller.signal });
+});
+
+
